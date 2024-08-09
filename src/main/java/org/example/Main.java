fb9912094f727b77c4ea007113a1453f7f7f6444
ChayLuck <barbarosuzunlar2003@gmail.com>
2024-08-09 15:38:45 +0300

    .

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -20,7 +20,7 @@ public class Main {
 
         final ActorRef akkaListener = system.actorOf(AkkaListener.props(), "akkaListener");
 
-        akkaListener.tell("{\"type\": \"voice\", \"senderMSISDN\": \"1234567890\", \"usageAmount\": 10}", ActorRef.noSender());
+        akkaListener.tell("{\"type\": \"voice\", \"senderMSISDN\": \"12345678901\", \"usageAmount\": 10}", ActorRef.noSender());
 
     }
 }
