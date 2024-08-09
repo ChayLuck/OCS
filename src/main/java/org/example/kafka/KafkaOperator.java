3d19ec4f0119f62e00b8af188a33d26ab8365e8e
ChayLuck <barbarosuzunlar2003@gmail.com>
2024-08-09 13:02:48 +0300

    .

--- src/main/java/org/example/kafka/KafkaOperator.java
+++ src/main/java/org/example/kafka/KafkaOperator.java
@@ -1,2 +1,12 @@
-package org.example.kafka;public class KafkaOperator {
+package org.example.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class KafkaOperator {
+
 }
